1e94bb6907513e74eec88b512313d6b1d49bb52d
kevin <kevinaltschuler@gmail.com>
2014-08-26 10:11:44 -0400

    wp-config

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'westside_wp');
+define('DB_NAME', 'tmsnyc');
 
 /** MySQL database username */
-define('DB_USER', 'westside_wp');
+define('DB_USER', 'albert');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'westsidenyc');
+define('DB_PASSWORD', 'cabbage');
 
 /** MySQL hostname */
-define('DB_HOST', 'westsidetmscenternyc.com');
+define('DB_HOST', 'halfgrain.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
