39f1c7b0fec98509e131c1cc310efbe0908125dd
kevtastic <kevinaltschuler@gmail.com>
2014-08-31 06:20:38 -0700

    first change of media location

--- wp-config.php
+++ wp-config.php
@@ -33,6 +33,14 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+
+/**
+ * MEDIA PATH
+ */
+define( 'UPLOADS', '/tmsnyc/wp-content/'.'files' );
+
+
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
